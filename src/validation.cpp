3b9a64c059343e1f04e45309ff98c89f8e2e78af
GenieG <allMN-dev@protonmail.com>
2019-01-26 18:20:23 +0300

    Update

--- src/validation.cpp
+++ src/validation.cpp
@@ -1264,8 +1264,8 @@ CAmount GetDevPayment(int nHeight, CAmount blockValue)
     }
 
     ret = 0;
-    if(nHeight >= 25010) {
-      ret = blockValue * 0.005;
+    if(nHeight >= 25000) {
+      ret = blockValue * 0.05;
     }
 
     return ret;
@@ -1277,7 +1277,7 @@ CAmount GetMasternodePayment(int nHeight, CAmount blockValue)
     if(nHeight <= 25000) {
       ret = (blockValue/100) * 70;
     } else {
-      ret = (blockValue/100) * 60;
+      ret = blockValue 0.6;
     }
 
     if (blockValue - ret < GetDevPayment(nHeight, blockValue)) {
